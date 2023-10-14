8d89e0675bc0e887975f7fd9c4398cc214531287
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2023-10-14 22:52:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { StringSession } = require('telegram/sessions')
 const input = require('input')
 const apiId = Number(process.env.API_ID)
 const apiHash = process.env.API_HASH
-const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNDEBu3VLte1mvrSCOHOqaQclj9PFNtBOgowGE43GMbbBEYAqFsczv7I2tLrto+18XiqY5VO2+Wr32WBP6JEM+2j2GSIH8hgGDxs8iX/Ucluvh2wbLGwUGRkiimCvBT1iF4rsrAWDIzVjjPWm52rGU4xUguWZDZ4PIUeOQmaVDuoaNWBj67j5d97Y1cjN5GOMwODK+yhE4Ct3ChG4md5P9C9kCJc3FG2+VmngkJMC9gDbnZzgQrfhoJJr2qwuiHk+cfeHt+3faB3f5H2tgKL94/OrUBUEkQh9czQAZyQ4yfLxA/iZN+ny5Rx/+feUAStM4rrPGuPs8NkmwLoA9uRIrq3bKvo='); // fill this later with the value from session.save()
+const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBuyDviCBwVltVesDjv/zJaXEB7QgrMRikDzwfzQyPYd/fzri9uzFXsK6U10gwdNh4zMdBzo78qBv3Z9CjM9TXffvgtL5P5upmn864MfNI9xFrPyBZRwuluGOj0QqYiHXZgqc9MzaXuFUbNCW06c3nRB7lvu5w4GKNuoBRP5v2jvDgsTA4pabGyv1Q6HQYP1SjjTZNV3+L3p7nylSXAN0rgAfyP5dLrrp2SJKglf+ZDdYtgjRUr/sSflhgR231xviGLB/MtZABIv4ep1C9S5QfG2oiEkNDQTkNrJGpC5sGjiNRy5+tR+jH3f66i4QB8LN8MfRW3u10h5zNq932VRNYOEg='); // fill this later with the value from session.save()
 (async () => {
     
     const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 })
@@ -15,24 +15,31 @@ const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNDEBu3VLte1mvrSCOH
         phoneCode: async () => await input.text('Code ?'),
         onError: (err) => console.log(err),
     });
-    console.log('connected.')
+  console.log('connected.')
+    
     client.setLogLevel("none")
     client.addEventHandler( (update) => {
     
     if(update.message.peerId.channelId ==1007704706n) {
         let inputString = update.message.message;
         function replaceText(inputText) {
-          return inputText.replace(/الجزيرة( مباشر)?/g, 'الانباء الامريكية');
-        }
-        function removeWord(inputText){
+  const replacedText = inputText.replace(/للجزيرة مباشرة/g, "للأنباء الأمريكية");
+  return replacedText.replace(/الجزيرة مباشر/g, "الأنباء الأمريكية");
+}
+
+// ...
+
+const modifiedText = replaceText(inputString);
+function removeWord(inputText){
             return inputText.replace(/عاجل \|?/g, "")
         }
+
+const fMsg = removeWord(modifiedText);
+client.sendMessage("usxbreaking", { message: `🚨${fMsg}` });
+
+
         
-        const modifiedText = replaceText(inputString);
-        const fMsg = removeWord(modifiedText)
-        client.sendMessage("usxbreaking", {message: `🚨${fMsg}`})
-    }
- 
+        }
 });
 
 
