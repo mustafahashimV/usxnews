bfa2c006ff810a867f0fa9c41ebff7095c23d3f5
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2023-10-15 01:07:05 +0300

    updated hash

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { StringSession } = require('telegram/sessions')
 const input = require('input')
 const apiId = Number(process.env.API_ID)
 const apiHash = process.env.API_HASH
-const stringSession = new StringSession(''); // fill this later with the value from session.save()
+const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu45+DxUf9oi5mz4OFPHreBI1X6awevbqBsvs9fdSs3TQAxzAWrmAx+3lVp2iSqwjGfzZ/yIbQgj5l5IXFw+ThC3SsnkE97yqNLUOrTpwyzmEOOGIc5wLa2cyRud8Jtgy/OWvP//pj2iY9lAv40+2MQr5CrQDxlRZvqdYwghFbJAD0n5fyMavSXcOZ7h4w4g7SN67Ab8vOpL7ihOXBzlBZD/bJF0Q0Sj6gPzmCObc5IfVHuiImz2JgW83ZYZXEmeCuqn+BahKgcUxwjZ/nxQFrwvp0LAEeTuI6G+IFCAtgDve7+8+ivCm8Z7LrGhGvqEjMjWls0DtW0kyAtQ4JFB/c3k='); // fill this later with the value from session.save()
 (async () => {
     
     const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 })
