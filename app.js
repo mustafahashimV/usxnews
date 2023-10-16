d5175a0689c52f31e474f2255a53bc996f605566
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2023-10-16 22:03:18 +0300

    t

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express()
 const apiId = Number(process.env.API_ID)
 const apiHash = process.env.API_HASH
 
-const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu45+DxUf9oi5mz4OFPHreBI1X6awevbqBsvs9fdSs3TQAxzAWrmAx+3lVp2iSqwjGfzZ/yIbQgj5l5IXFw+ThC3SsnkE97yqNLUOrTpwyzmEOOGIc5wLa2cyRud8Jtgy/OWvP//pj2iY9lAv40+2MQr5CrQDxlRZvqdYwghFbJAD0n5fyMavSXcOZ7h4w4g7SN67Ab8vOpL7ihOXBzlBZD/bJF0Q0Sj6gPzmCObc5IfVHuiImz2JgW83ZYZXEmeCuqn+BahKgcUxwjZ/nxQFrwvp0LAEeTuI6G+IFCAtgDve7+8+ivCm8Z7LrGhGvqEjMjWls0DtW0kyAtQ4JFB/c3k=');
-
+const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu45+DxUf9oi5mz4OFPHreBI1X6awevbqBsvs9fdSs3TQAxzAWrmAx+3lVp2iSqwjGfzZ/yIbQgj5l5IXFw+ThC3SsnkE97yqNLUOrTpwyzmEOOGIc5wLa2cyRud8Jtgy/OWvP//pj2iY9lAv40+2MQr5CrQDxlRZvqdYwghFbJAD0n5fyMavSXcOZ7h4w4g7SN67Ab8vOpL7ihOXBzlBZD/bJF0Q0Sj6gPzmCObc5IfVHuiImz2JgW83ZYZXEmeCuqn+BahKgcUxwjZ/nxQFrwvp0LAEeTuI6G+IFCAtgDve7+8+ivCm8Z7LrGhGvqEjMjWls0DtW0kyAtQ4JFB/c3k='); // fill this later with the value from session.save()
 (async () => {
+    
     const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 })
     await client.start({
         phoneNumber: process.env.PHONE,
@@ -17,25 +17,31 @@ const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu45+DxUf9oi5mz
         phoneCode: async () => await input.text('Code ?'),
         onError: (err) => console.log(err),
     });
-    console.log("connected.")
+  console.log('connected.')
+    client.sendMessage("me", {message: client.session.save()})
+    client.setLogLevel("none")
+    client.addEventHandler( (update) => {
     
-    client.sendMessage("me", { message: client.session.save() });
-    client.setLogLevel("none");
-    client.addEventHandler((update) => {
-        if (update.message.peerId.channelId === 1007704706n) {
-            client.sendMessage("me", {message: update.message.message})
-            const modifiedMessage = update.message.message.replace(/للجزيرة مباشر/g, "للأنباء الأمريكية");
-            const withoutUrgent = modifiedMessage.replace(/عاجل \|?/g, "");
-            const containsLink = /https?:\/\/\S+/i.test(withoutUrgent);
-            
-                client.sendMessage("usxbreaking", { message: `🚨${withoutUrgent}` });
-            
+    if(update.message.peerId.channelId ==1007704706n) {
+        let inputString = update.message.message;
+        function replaceText(inputText) {
+  const replacedText = inputText.replace(/للجزيرة مباشرة/g, "للأنباء الأمريكية");
+  return replacedText.replace(/الجزيرة مباشر/g, "الأنباء الأمريكية");
+}
+
+const modifiedText = replaceText(inputString);
+function removeWord(inputText){
+            return inputText.replace(/عاجل \|?/g, "")
         }
-    });
-})();
+
+const fMsg = removeWord(modifiedText);
+client.sendMessage("usxbreaking", { message: `🚨${fMsg}` });
+        }
+});
+})()
 
 app.get("/check", (req, res) => {
-    res.status(200).json("check passed!");
-});
+  res.status(200).json("Check Passed!")
+})
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(3000, ()=> console.log("listening..8080"))
