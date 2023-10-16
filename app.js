9ac19f696e8807e1a30c4974a29e12ceceb22a20
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2023-10-16 20:22:49 +0300

    updated

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const dotenv = require("dotenv").config()
 const { TelegramClient } = require('telegram')
 const { StringSession } = require('telegram/sessions')
 const input = require('input')
-const express = require("express")
-const app = express()
-const job = require("./cron.js").job
+
 const apiId = Number(process.env.API_ID)
 const apiHash = process.env.API_HASH
 const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu45+DxUf9oi5mz4OFPHreBI1X6awevbqBsvs9fdSs3TQAxzAWrmAx+3lVp2iSqwjGfzZ/yIbQgj5l5IXFw+ThC3SsnkE97yqNLUOrTpwyzmEOOGIc5wLa2cyRud8Jtgy/OWvP//pj2iY9lAv40+2MQr5CrQDxlRZvqdYwghFbJAD0n5fyMavSXcOZ7h4w4g7SN67Ab8vOpL7ihOXBzlBZD/bJF0Q0Sj6gPzmCObc5IfVHuiImz2JgW83ZYZXEmeCuqn+BahKgcUxwjZ/nxQFrwvp0LAEeTuI6G+IFCAtgDve7+8+ivCm8Z7LrGhGvqEjMjWls0DtW0kyAtQ4JFB/c3k='); // fill this later with the value from session.save()
@@ -39,10 +37,3 @@ client.sendMessage("usxbreaking", { message: `🚨${fMsg}` });
         }
 });
 })()
-
-app.get("/check", (req, res) => {
-  res.send("<p>Check Passed!</p>")
-})
-
-app.listen(8080, ()=> console.log("listening..8080"))
-job.start();
