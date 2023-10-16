3ec18bc0c94a13f577b27dbdcf92f30ec68f533b
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2023-10-16 21:41:55 +0300

    gg

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu45+DxUf9oi5mz
 })();
 
 app.get("/check", (req, res) => {
-    res.send("<p>Check Passed!</p>");
+    res.status(200).send("<p>Check Passed!</p>").json("check passed!");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
