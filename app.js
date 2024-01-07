bfcb67dbc98ffff90b5cf805ac6b5b68770d3901
Mustafa Hashim <www.pqowieuryt0@gmail.com>
2024-01-07 21:11:21 +0000

    bb

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-const { TelegramClient } = require('telegram');
-const { StringSession } = require('telegram/sessions');
-const input = require('input');
-const translatte = require("translatte");
 
-// Replace placeholders with your sensitive information
-const apiId = process.env.API_ID;
+
+const dotenv = require("dotenv").config();
+const { Api, TelegramClient } = require('telegram');
+const { StringSession } = require('telegram/sessions');
+const input = require('input')
+const translatte = require("translatte");
+const express = require('express');
+const app = express();
+
+const apiId = Number(process.env.API_ID);
 const apiHash = process.env.API_HASH;
-const stringSession = new StringSession(process.env.STRING_SESSION);
+
+const filter = require("./filters.js");
+
+const stringSession = new StringSession(process.env.STRING_SESSION);  
 
 (async () => {
     const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 })
-
-    if (client.isConnected()) {
-        await client.logOut();
-        console.log('Logged out of the previous session.');
-    }
-
     await client.start({
         phoneNumber: process.env.PHONE,
         password: async () => await input.text('password?'),
         phoneCode: async () => await input.text('Code ?'),
         onError: (err) => console.log(err),
     });
-
-    console.log('Connected.');
-
-    client.sendMessage("me", { message: client.session.save() });
+ 
+    console.log('connected.');
+    client.sendMessage("me", {message: client.session.save()});
     client.setLogLevel("none");
     
     client.addEventHandler(async (update) => {
+
         let mText = await filter.filter(update.message.message);
         update.message.message = mText;
         var messageT = update.message
@@ -43,16 +44,25 @@ const stringSession = new StringSession(process.env.STRING_SESSION);
         let testCh = 1959122209n;
 
         if (channelSourceId == aljazeersId) {
-            const translatedMessage = await translatte(mText, { to: "en" }).then(async res => {
+    
+                const translatedMessage = await translatte(mText, { to: "en" }).then(async res => {
                 await client.sendMessage("usxnews_en", { message: res.text })
-            }).catch(error => {
-                client.sendMessage("me", { message: error });
-            });
-        }
+                }).catch(error => {
+                    client.sendMessage("me", { message: error });
+                });
+            }
 
-        if (channelSourceId == aljazeersId) {
+        if(channelSourceId == aljazeersId) {
             await post(aljazeersId, "usxbreaking", false);
         }
 
     });
 })();
+
+app.get("/check", (req, res) => {
+    res.status(200).json("Check Passed!");
+});
+
+app.listen(3000, () => {
+    console.log("3000P")
+});
